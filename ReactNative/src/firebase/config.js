10575b33e2abcf8b7ae16606515873da9afa1e43
Serena Dominguez <seredominguez@hotmail.com>
2021-11-15 16:35:15 -0300

    creando carpetas y agregando firebase

---
+++ ReactNative/src/firebase/config.js
@@ -0,0 +1,19 @@
+import app from "firebase/app";
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCparhE5oFj4NFxL3ZfR03z4idux04acH4",
+    authDomain: "proyectointegrador-reactnative.firebaseapp.com",
+    projectId: "proyectointegrador-reactnative",
+    storageBucket: "proyectointegrador-reactnative.appspot.com",
+    messagingSenderId: "537554396751",
+    appId: "1:537554396751:web:fd67736cefd13dad77ec3a"
+  };
+
+  app.initializeApp(firebaseConfig);
+
+  export const auth = firebase.auth();
+  export const storage = firebase.storage();
+  export const db = firebase.firestore();
+  
+//   hola
